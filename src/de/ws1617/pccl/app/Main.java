e865905b4fba0d67e61ad917dd2f30db2a2d9f38
Johannes <Joapfel@Michaela-Lutz.local>
2016-12-21 14:25:41 +0100

    task without solution

---
+++ src/de/ws1617/pccl/app/Main.java
@@ -0,0 +1,22 @@
+package de.ws1617.pccl.app;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import de.ws1617.pccl.grammar.Grammar;
+import de.ws1617.pccl.grammar.GrammarUtils;
+import de.ws1617.pccl.grammar.Lexicon;
+import de.ws1617.pccl.grammar.NonTerminal;
+import de.ws1617.pccl.grammar.Symbol;
+import de.ws1617.pccl.grammar.Terminal;
+import de.ws1617.pccl.parser.CYK;
+import de.ws1617.pccl.tree.Tree;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// TODO instantiate a CYK parser and parse a sentence
+
+	}
+}
---
+++ src/de/ws1617/pccl/parser/CYK.java
@@ -0,0 +1,7 @@
+package de.ws1617.pccl.parser;
+
+public class CYK {
+
+	// TODO implement me !
+
+}
